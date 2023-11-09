00c35e896b3f71ff83185412a30339150620f179
Kamyab Nazari <kamyabnazari@icloud.com>
2023-11-10 00:45:52 +0100

    Updating error handling in generate_assistant

--- backend/app/routers/api.py
+++ backend/app/routers/api.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter, HTTPException
 from app.models.pydantic_models import GenerateMessageRequest, GenerateAssistantRequest
-from openai import OpenAI
+from openai import OpenAI, OpenAIError
 from app.core.config import settings
 import time
 
 router = APIRouter()
-
 client = OpenAI(api_key=settings.openai_api_key)
 
 @router.get("/")
@@ -23,57 +22,46 @@ async def generate_message(request_body: GenerateMessageRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+async def poll_for_completion(thread_id, run_id, timeout=60):
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        run_status = client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run_id)
+        if run_status.status == "completed":
+            return True
+        elif run_status.status in ["failed", "cancelled", "expired"]:
+            return False
+        time.sleep(1)
+    return False
+
 @router.post("/generate_assistant")
 async def generate_assistant(request_body: GenerateAssistantRequest):
     try:
-        # Step 1: Create an Assistant
         assistant = client.beta.assistants.create(
             name=request_body.name,
             instructions=request_body.instructions,
             tools=[{"type": "code_interpreter"}],
             model="gpt-3.5-turbo"
         )
-
-        # Step 2: Create a Thread
         thread = client.beta.threads.create()
-
-        # Step 3: Add a Message to a Thread
         message = client.beta.threads.messages.create(
             thread_id=thread.id,
             role="user",
             content=request_body.initial_message
         )
-
-        # Step 4: Run the Assistant
         run = client.beta.threads.runs.create(
             thread_id=thread.id,
             assistant_id=assistant.id
         )
+        if not await poll_for_completion(thread.id, run.id):
+            raise HTTPException(status_code=500, detail="Assistant run did not complete in time.")
 
-        # Step 5: Poll for the run completion and retrieve messages
-        while True:
-            run_status = client.beta.threads.runs.retrieve(
-                thread_id=thread.id,
-                run_id=run.id
-            )
-            if run_status.status == "completed":
-                break
-            time.sleep(1)  # Sleep to avoid rate limits
-
-        # Retrieve messages after the run is completed
-        messages_response = client.beta.threads.messages.list(
-            thread_id=thread.id
-        )
-
-        # Extract the Assistant's messages
+        messages_response = client.beta.threads.messages.list(thread_id=thread.id)
         assistant_messages = [
-            content_piece.text.value
-            for message in messages_response.data
-            if message.role == "assistant"
-            for content_piece in message.content
-            if content_piece.type == 'text' and hasattr(content_piece, 'text')
+            cp.text.value for msg in messages_response.data if msg.role == "assistant"
+            for cp in msg.content if cp.type == 'text' and hasattr(cp, 'text')
         ]
-
         return {"messages": assistant_messages}
-    except Exception as e:
+    except OpenAIError as e:
         raise HTTPException(status_code=500, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="An unexpected error occurred.")
