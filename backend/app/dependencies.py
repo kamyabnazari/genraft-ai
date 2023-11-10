2d658c4b5465eb7ad126a08889ba3ed352039a05
Kamyab Nazari <kamyabnazari@icloud.com>
2023-11-10 11:21:39 +0100

    Adding sqlite .db ignores and setup

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -2,17 +2,36 @@ from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from databases import Database
+import logging
+import os
 
-DATABASE_URL = "sqlite:///./mydatabase.db"
+# Create the Genraft AI data folder
+folder_path = './genraft_ai_data'
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+# Create database instance
+DATABASE_URL = "sqlite:///./genraft_ai_data/genraftai.db"
 database = Database(DATABASE_URL)
 
+# Connect to the database instance and create the .db file
 @asynccontextmanager
 async def lifespan(app):
-    # Startup logic: connect to the database
-    await database.connect()
+    try:
+        # Startup logic: connect to the database
+        await database.connect()
+        query = "CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name TEXT)"
+        await database.execute(query)
+    except Exception as e:
+        logging.error(f"Failed to connect to the database: {e}")
+        raise
     yield
-    # Shutdown logic: disconnect from the database
-    await database.disconnect()
+    try:
+        # Shutdown logic: disconnect from the database
+        await database.disconnect()
+    except Exception as e:
+        logging.error(f"Failed to disconnect from the database: {e}")
+        raise
 
 def configure_cors(app):
     app.add_middleware(
